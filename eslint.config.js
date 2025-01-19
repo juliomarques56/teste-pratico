1e7faaf8b4328bee32ebb73d4a0d55c17e583473
julio.marques <julio_cmarques@sicredi.com.br>
2025-01-19 13:38:22 -0300

    feat: configura analise de commit padrao

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,14 @@
-// eslint.config.js
-
 const eslintPluginPrettier = require('eslint-plugin-prettier');
 const eslintConfigPrettier = require('eslint-config-prettier');
+const eslintPluginCypress = require('eslint-plugin-cypress');
+const globals = require('globals');
 
 module.exports = [
   {
+    ignores: ['**/node_modules/', 'cypress/videos/', 'cypress/screenshots/', '**/screenshots/', '**/allure-report/', '**/allure-results/'],
+  },
+  {
+
     files: ['*.js', '*.ts'],
     languageOptions: {
       parserOptions: {
@@ -15,18 +19,27 @@ module.exports = [
         browser: true,
         node: true,
       },
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        cy: 'readonly',
+        Cypress: 'readonly',
+        describe: 'readonly',
+        it: 'readonly',
+        before: 'readonly',
+        after: 'readonly',
+      },
     },
     plugins: {
       prettier: eslintPluginPrettier,
+      cypress: eslintPluginCypress,
     },
     rules: {
       'prettier/prettier': 'error',
-    },
-  },
-  {
-    files: ['*.js'],
-    rules: {
       'no-unused-vars': 'warn',
+      'no-console': 'off',
+      'cypress/no-unnecessary-waiting': 'warn',
     },
+    extends: [eslintConfigPrettier],
   },
 ];
