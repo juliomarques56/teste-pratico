8ee4e5a9b04291196dd5d43fecb89c72ed4dbc6b
julio.marques <julio_cmarques@sicredi.com.br>
2025-01-19 00:41:16 -0300

    feat: mapeamento de seletores

---
+++ cypress/support/selectors/loanPageSelectors.js
@@ -0,0 +1,16 @@
+export const loanPageSelectors = {
+    pageTitle: 'div > h1',
+    pageSubtitle: 'div > h2',
+    cpfInstruction: 'form > div > p',
+    cpfField: '#mat-input-0',
+    accessButton: 'button[type="submit"]',
+    outline: 'mat-form-field',
+    errorMessage: '#mat-mdc-error-0',
+    helpCenterText: 'span.text-gray-900',
+    helpCenterLink: 'a',
+    image: 'img.h-full.w-full.object-cover.object-right-top',
+}
+
+export const supportPageSelectors = {
+    acceptCookies: '#onetrust-accept-btn-handler',
+};
