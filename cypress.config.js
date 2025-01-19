b002f8b0763f394354672d21c8baa864230d6028
julio.marques <julio_cmarques@sicredi.com.br>
2025-01-19 00:52:59 -0300

    feat: configura relatorio de execucao

--- cypress.config.js
+++ cypress.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require("cypress");
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 
 module.exports = defineConfig({
   e2e: {
@@ -10,7 +11,8 @@ module.exports = defineConfig({
     screenshotsFolder: 'screenshots/',
     experimentalOriginDependencies: true,
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      allureWriter(on, config);
+      return config;
     },
   },
 });
